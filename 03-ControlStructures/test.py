385737680195cc1a5ff8c73de7f25c8a044e84ba
Wiktoria Żełubowska <zelubowska.wiktoria@gmail.com>
2025-10-30 20:03:39 +0100

    Add files via upload

---
+++ 03-ControlStructures/test.py
@@ -0,0 +1,13 @@
+#print("Hello")
+#abc = 2+3
+#print(abc)
+
+age = 23
+print(age)
+print("My age is", age)
+print(f"My age is {age+15}")
+
+university = input("Your university: ")
+
+age = input("Your age: ")
+age = int(age)
